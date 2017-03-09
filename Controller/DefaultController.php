ff1cbd3caa4eeb628b957bf6b4203be756752595
Thomas Hanke <thomas@it-hanke.de>
2017-03-09 11:52:11 +0100

    add download-function

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace SN\BackupBundle\Controller;
 
 use Gaufrette\Exception\FileNotFound;
+use SN\BackupBundle\Model\BackupList;
 use SN\ToolboxBundle\Helper\CommandHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Filesystem\Filesystem;
@@ -20,7 +21,7 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $config  = $this->getParameter('sn_backup');
-        $backups = json_decode($this->getLocalConfig($config["backup_folder"]), true)["dumps"];
+        $backups = BackupList::factory()->getDumps();
 
         return $this->render('SNBackupBundle:Default:index.html.twig',
             array(
@@ -35,15 +36,13 @@ class DefaultController extends Controller
      */
     public function downloadAction(Request $request)
     {
-        $config      = $this->getParameter('sn_backup');
-        $backups     = json_decode($this->getLocalConfig($config["backup_folder"]), true)["dumps"];
-        $backup      = $backups[$request->get('id')];
-        $archiveName = sprintf("%s.tar.gz", date("Y-m-d_H-i-s", $backup["timestamp"]));
+        $list   = BackupList::factory();
+        $backup = $list->getDumps()[$request->get('id')];
 
-        $response = new BinaryFileResponse($this->getFile($config["backup_folder"], $archiveName));
+        $response = new BinaryFileResponse($backup->getFile());
         $response->setContentDisposition(
             ResponseHeaderBag::DISPOSITION_INLINE,
-            $archiveName
+            $backup->getFilename()
         );
 
         return $response;
@@ -56,100 +55,26 @@ class DefaultController extends Controller
     {
 
         $config      = $this->getParameter('sn_backup');
-        $backups     = json_decode($this->getLocalConfig($config["backup_folder"]), true)["dumps"];
-        $backup      = $backups[$request->get('id')];
-        $archiveName = sprintf("%s.tar.gz", date("Y-m-d_H-i-s", $backup["timestamp"]));
-        $tmp         = sprintf("/tmp/%s", $archiveName);
+        $list = BackupList::factory();
+        $archive      = $list->getDumps()[$request->get('id')];
+        $tmp         = sprintf("/tmp/%s", $archive->getFilename());
 
-        $this->copyFromBackup($config["backup_folder"], $archiveName, $tmp);
+        $fs = new Filesystem();
+        $fs->copy($archive->getFile(), $tmp);
 
         $filelist = CommandHelper::executeCommand(sprintf("tar -tf %s",
             $tmp
         ));
 
-        $fs = new Filesystem();
         $fs->remove($tmp);
 
         $backup["files"] = explode("\n", $filelist);
-        $backup["name"]  = $archiveName;
+        $backup["name"]  = $archive->getFilename();
 
 
         return $this->render('SNBackupBundle:Default:info.html.twig',
             array(
                 "backup" => $backup
             ));
-    }
-
-
-    /**
-     * @param $backupFolder
-     * @param $source
-     * @param $target
-     */
-    protected function copyFromBackup($backupFolder, $source, $target)
-    {
-        $backupArchive = sprintf("%s/%s", $backupFolder, $source);
-
-        try {
-            /**
-             * @var $gfs \Gaufrette\Filesystem
-             */
-            $gfs  = $this
-                ->get('knp_gaufrette.filesystem_map')
-                ->get($backupFolder);
-            $data = $gfs->read($source);
-
-            /**
-             * @var $fs Filesystem
-             */
-            $fs = new Filesystem();
-            $fs->dumpFile($target, $data);
-        } catch (\InvalidArgumentException $exception) {
-            CommandHelper::executeCommand(sprintf("cp %s %s", $backupArchive, $target));
-        }
-    }
-
-    protected function getFile($backupFolder, $source)
-    {
-        $backupArchive = sprintf("%s/%s", $backupFolder, $source);
-
-        try {
-            /**
-             * @var $gfs \Gaufrette\Filesystem
-             */
-            $gfs  = $this
-                ->get('knp_gaufrette.filesystem_map')
-                ->get($backupFolder);
-            return $gfs->get($backupArchive);
-
-        } catch (\InvalidArgumentException $exception) {
-            return $backupArchive;
-        }
-    }
-
-    /**
-     * @return string
-     */
-    protected function getLocalConfig($backupFolder)
-    {
-        try {
-            /**
-             * @var $fs \Gaufrette\Filesystem
-             */
-            $fs = $this->get('knp_gaufrette.filesystem_map')
-                ->get($backupFolder);
-            try {
-                return $fs->read('backup.json');
-            } catch (FileNotFound $exception) {
-                return "{dumps:[]}";
-            }
-        } catch (\InvalidArgumentException $exception) {
-            $backupFile = sprintf("%s/backup.json", $backupFolder);
-            if (file_exists($backupFile)) {
-                return file_get_contents($backupFile);
-            } else {
-                return "{dumps:[]}";
-            }
-        }
     }
 }
